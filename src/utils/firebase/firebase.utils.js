b3633daa563d308ed9e29a153be0a3cc20e65fe8
adrianmdev <adrianmdevelopment@gmail.com>
2024-01-09 11:20:42 -0500

    firebase authentication stage of project. Implementation of googlePopUp

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,40 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7ESQcEHSS0PvtGrE5FK5A5ZIGoExcxo0",
+    authDomain: "ecommerce-ba31e.firebaseapp.com",
+    projectId: "ecommerce-ba31e",
+    storageBucket: "ecommerce-ba31e.appspot.com",
+    messagingSenderId: "1066511802317",
+    appId: "1:1066511802317:web:3daa9368b121a66418ee84"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+}
